4ea22747e37c6a32aa900421df01d9b970f5e94a
ryosuke <ryskosn@gmail.com>
2017-01-14 18:36:47 +0900

    add: test task using power-assert

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,17 @@
 var gulp = require('gulp');
+var mocha = require('gulp-mocha');
+
 gulp.task('hello', function() { console.log('Hello gulp!'); });
 gulp.task('default', ['hello']);
+
+gulp.task('test_raw', function() {
+  gulp.src('./test/*.js', {read: false}).pipe(mocha());
+});
+
+gulp.task('test', function() {
+  gulp.src('./test/*.js', {read: false}).pipe(mocha({
+    require: ['intelli-espower-loader']
+  }));
+});
+
+gulp.task('watch', function() { gulp.watch('./test/*.js', ['test']); });
