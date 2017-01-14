630968aeb18b334ac98105bb3ec156c593d88781
ryosuke <ryskosn@gmail.com>
2017-01-14 19:31:01 +0900

    add: variable paths for structured style

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,16 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
-
-gulp.task('hello', function() { console.log('Hello gulp!'); });
-gulp.task('default', ['hello']);
+var paths = {test: "./test/*.js"};
 
 gulp.task('test_raw', function() {
   gulp.src('./test/*.js', {read: false}).pipe(mocha());
 });
 
 gulp.task('test', function() {
-  gulp.src('./test/*.js', {read: false}).pipe(mocha({
+  gulp.src(paths.test, {read: false}).pipe(mocha({
     // for power-assert
     require: ['intelli-espower-loader']
   }));
 });
 
-gulp.task('watch', function() { gulp.watch('./test/*.js', ['test']); });
+gulp.task('watch', function() { gulp.watch(paths.test, ['test']); });
